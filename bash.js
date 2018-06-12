8cb08c05831461a68660306c95289b2f0ce16380
Cindy Song <ydnic622@gmail.com>
2018-06-12 13:18:10 -0400

    ls

--- bash.js
+++ bash.js
@@ -1,5 +1,6 @@
 process.stdout.write('prompt > ');
 
 const pwd = require('./pwd');
-
+const ls = require('./ls')
 pwd()
+ls()
---
+++ ls.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+module.exports = function() {
+  process.stdin.on('data', data => {
+
+
+    if (data.toString().trim() === 'ls') {
+      fs.readdir('./', 'utf8', (err, files) => {
+        if (err) {
+          throw err;
+        } else {
+          process.stdout.write(files.join('\n'));
+          process.stdout.write('\nprompt >');
+        }
+      });
+    }
+
+
+
+  });
+};
